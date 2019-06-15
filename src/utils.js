83b789ad55e2fcc2074bf635a7149a1d2ad54666
Ihor Krupin <brain0verf0wz@gmail.com>
2019-06-15 12:50:42 +0300

    Graphics branch images.

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export const splitArrayToChunks = (arr, size = 25) => {
-    const myArray = [];
-    for(let i = 0; i < arr.length; i += size) {
-        myArray.push(arr.slice(i, i+size));
-    }
-    return myArray;
-};
